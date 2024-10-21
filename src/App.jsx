08bfe3b65e69e9af038e48691696ec1ced6935c3
Arpit Nigam <arpitnigam2412@gmail.com>
2024-10-21 21:10:48 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Header from './components/header/Header';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Verify from './pages/auth/Verify';
-import Footer from './components/footer/footer';
+import Footer from './components/footer/Footer';
 import About from './pages/about/About';
 import Account from './pages/account/Account';
 import { UserData } from './context/UserContext';
